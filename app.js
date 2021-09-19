61f5fda52d2b419ba4fa2287306c35f62574766a
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-19 14:20:21 -0400

    add express set up

---
+++ app.js
@@ -0,0 +1,29 @@
+// TODO: create JSON API application to store a shopping list (Array)
+// 1. each item is an object
+// [{ name: price}, {name2: price2}]
+
+const express = require("express");
+const app = express();
+const itemsRoutes = require("./routes/items");
+const ExpressError = require("./expressError");
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+    return new ExpressError("Not Found", 404);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    return res.json({
+        error: err.message,
+    });
+});
+
+module.exports = app;
--- routes/items.js
+++ routes/items.js
@@ -1,29 +1,33 @@
+const express = require("express");
+
 const router = new express.Router();
 // app.js app.use with prefix routes with /items, so these routes do not need /items
 
 //1. this should render a list of shopping items
-router.get("/", () => {});
+router.get("/", (req) => {});
 
 // Here is what a response looks like:
 // [{“name”: “popsicle”, “price”: 1.45}, {“name”:”cheerios”, “price”: 3.40}]
 
 //2. this route should accept JSON data and add it to the shopping list.
-router.post("/", () => {});
+router.post("/", (req) => {});
 
 // Here is what a sample request/response looks like:
 // {“name”:”popsicle”, “price”: 1.45} => {“added”: {“name”: “popsicle”, “price”: 1.45}}
 
 //3. this route should display a single item’s name and price.
-router.get("/", () => {});
+router.get("/", (req) => {});
 // Here is what a sample response looks like:
 // {“name”: “popsicle”, “price”: 1.45}
 
 //4.  this route should modify a single item’s name and/or price.
-router.patch("/:name", () => {});
+router.patch("/:name", (req) => {});
 // Here is what a sample request/response looks like:
 // {“name”:”new popsicle”, “price”: 2.45} => {“updated”: {“name”: “new popsicle”, “price”: 2.45}}
 
 //5. this route should allow you to delete a specific item from the array.
-router.delete("/:name", () => {});
+router.delete("/:name", (req) => {});
 // Here is what a sample response looks like:
 // {message: “Deleted”}
+
+module.exports = router;
